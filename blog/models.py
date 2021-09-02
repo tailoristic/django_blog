4a8a0b545d3c9e32b5d2a4d154f75357ea63a1f6
Xira Infotech <75717852+xirainfotech@users.noreply.github.com>
2021-09-02 10:20:26 +0530

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -3,4 +3,5 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=150)
     desc = models.TextField()
-    
+    user_id = models.IntegerField(default=0)
+    user_name = models.CharField(max_length=150, default="")
